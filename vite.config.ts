6ef5541ab08a864fe9faf4c32d8372305176420f
vladyffka <vzalesinski@gmail.com>
2024-06-12 13:00:18 +0400

    feat: added pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
+const vitePWA = VitePWA({
+  registerType: 'autoUpdate',
+  outDir: 'dist',
+  manifest: {
+    name: 'vite-pwa',
+    short_name: 'vitePWA',
+    description: 'vitejs example build',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: 'assets/images/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: 'assets/images/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+});
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vitePWA],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
---
+++ workbox-config.js
@@ -0,0 +1,5 @@
+export default {
+  globDirectory: 'dist/',
+  globPatters: ['**/*/{css,woff2,png,jpeg,jpg,js,svg}'],
+  swDest: 'dist/sw.js',
+};
